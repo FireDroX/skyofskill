306f46d9340469f1daa8044d690d14513908c170
FireDroX <83990394+FireDroX@users.noreply.github.com>
2024-02-17 14:31:58 +0100

    Update mobile rendering

--- src/pages/minage/Minage.jsx
+++ src/pages/minage/Minage.jsx
@@ -1,36 +1,11 @@
 import './Minage.css';
+import { compactNumber, formatNumberWithSpaces } from "../../utils/functions";
 
 import { useState } from 'react';
 import { FaCoins } from "react-icons/fa6";
 
+
 const { f0 } = require('../../utils/mines.json');
-
-const compactNumber = (number = Number) => {
-  const suffixes = ["", "K", "M", "B", "T", "Q"];
-  let suffixNum = Math.floor(("" + number).length / 3);
-  
-  let shortValue = parseFloat((suffixNum !== 0 ? (number / Math.pow(1000, suffixNum)) : number).toPrecision(4));
-  
-  if (shortValue % 1 !== 0) {
-      shortValue = shortValue.toFixed(3);
-  }
-  
-  if (shortValue < 1 && suffixNum > 0) {
-      shortValue *= 1000;
-      suffixNum--;
-  }
-
-  return shortValue + suffixes[suffixNum];
-};
-
-const formatNumberWithSpaces = (number = 0) => {
-  const numberString = number.toString();
-  const groups = [];
-  for (let i = numberString.length; i > 0; i -= 3) {
-    groups.unshift(numberString.substring(Math.max(0, i - 3), i));
-  }
-  return groups.join(' ');
-}
 
 const Minage = () => {
   const prestiges = Array.from({length: 17}, (_, i) => "Prestige " + (i * 5 + 5));
