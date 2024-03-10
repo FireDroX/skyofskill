6e0d7c7b693817650997e5405b1c99c80bd275ab
FireDroX <83990394+FireDroX@users.noreply.github.com>
2024-03-10 13:37:20 +0100

    Create InputValues.jsx ✨

--- src/pages/minage/Minage.jsx
+++ src/pages/minage/Minage.jsx
@@ -7,6 +7,8 @@ import { FaCoins } from "react-icons/fa6";
 
 import { UserContext } from "../../utils/UserContext";
 
+import InputValues from "../../components/InputValues/InputValues";
+
 const Minage = () => {
   const {
     fortune,
@@ -23,23 +25,6 @@ const Minage = () => {
   for (let i = 0; i < mines.length; i += 9) {
     namesTable.push(mines.slice(i, i + 9));
   }
-
-  const handleInputChange = (e, v) => {
-    let newValue = e.target.value.replace(/[^0-9]/g, "");
-    if (newValue === "") newValue = 1;
-    if (newValue > 999999) newValue = 999999;
-
-    switch (v) {
-      case "fortune":
-        setFortune(Number(newValue));
-        break;
-      case "blocs":
-        setBlocs(Number(newValue));
-        break;
-      default:
-        break;
-    }
-  };
 
   const CalcValues = () => {
     const bloc = Math.round(
@@ -88,54 +73,16 @@ const Minage = () => {
     <section className="App">
       <div>
         <div className="minage-values">
-          <div className="input-values">
-            <div>
-              <small>Fortune de votre pioche</small>
-              <input
-                name="Fortune de votre pioche"
-                type="text"
-                value={fortune}
-                onChange={(e) => handleInputChange(e, "fortune")}
-              />
-            </div>
-            <div>
-              <small>Votre mine prestige</small>
-              <select
-                name="Votre mine prestige"
-                defaultValue={mine}
-                onChange={(e) => setMine(e.target.value)}
-              >
-                {mines.map((mine, index) => (
-                  <option key={index} value={index}>
-                    {mine.name}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div>
-              <small>Votre boost de minage</small>
-              <select
-                name="Votre boost de minage"
-                defaultValue={boost}
-                onChange={(e) => setBoost(e.target.value)}
-              >
-                {boosts.map((b, index) => (
-                  <option key={index} value={index}>
-                    {b.name}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div>
-              <small>Blocs cassé / 15 minutes</small>
-              <input
-                name="Blocs cassé / 15 minutes"
-                type="text"
-                value={blocs}
-                onChange={(e) => handleInputChange(e, "blocs")}
-              />
-            </div>
-          </div>
+          <InputValues
+            fortune={fortune}
+            setFortune={setFortune}
+            blocs={blocs}
+            setBlocs={setBlocs}
+            mine={mine}
+            setMine={setMine}
+            boost={boost}
+            setBoost={setBoost}
+          />
           <div className="calc-values">
             <CalcValues />
           </div>
