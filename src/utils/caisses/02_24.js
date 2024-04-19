20ca37ea780acbb4b7a388a1b65c19e57ae86b00
FireDroX <83990394+FireDroX@users.noreply.github.com>
2024-04-19 18:36:53 +0200

    Update 🔨 : Colored Text 01_24 -> 03_24 🎨

--- src/utils/caisses/02_24.js
+++ src/utils/caisses/02_24.js
@@ -13,6 +13,7 @@ const caisse = {
   title: "MILLIONNAIRE",
   items: [
     {
+      name: "&8&kt&r&f☾&r&a۞&r&f☽&r&8&kt&r&8&l A&r&a&lr&r&8&lc &r&a&lS&r&8&lu&r&a&lp&r&8&lr&r&a&lê&r&8&lm&r&a&le &r&8&kt&r&f☾&r&a۞&r&f☽&r&8&kt&r",
       defaultName: "Arc Suprême",
       type: 6,
       enchants: [
@@ -22,6 +23,7 @@ const caisse = {
       ],
     },
     {
+      name: "&f&kt&r&4​☸&r&c࿊&r&4​☸&r&f&kt&r&4&l P&r&c&lr&r&4&lo&r&c&lp&r&4&lu&r&c&ll&r&4&ls&r&c&le&r&4&lu&r&c&lr&r&4&l d&r&c&le&r&4&l R&r&c&lu&r&4&lb&r&c&li&r&4&ls &r&f&kt&r&4​☸&r&c࿊&r&4​☸&r&f&kt&r",
       defaultName: "Propulseur de Rubis",
       type: 6,
       enchants: [
@@ -41,6 +43,7 @@ const caisse = {
       dontLeaveOnDeath: true,
     },
     {
+      name: "&f&kt&r&a◧&r&f❖&r&a◧&r&f&kt&r&a&l C&r&f&lo&r&a&lu&r&f&lr&r&a&lb&r&f&le&r&a&l d&r&f&le&r&a&l S&r&f&la&r&a&lp&r&f&lh&r&a&li&r&f&lr &r&f&kt&r&a◧&r&f❖&r&a◧&r&f&kt&r",
       defaultName: "Courbe de Saphir",
       type: 6,
       enchants: [
@@ -61,6 +64,7 @@ const caisse = {
       dontLeaveOnDeath: true,
     },
     {
+      name: "&8&kt&r&f☾&r&e۞&r&f☽&r&8&kt&r&e&l L&r&8&la&r&e&lm&r&8&le &r&e&lS&r&8&lu&r&e&lp&r&8&lr&r&e&lê&r&8&lm&r&e&le &r&8&kt&r&f☾&r&e۞&r&f☽&r&8&kt&r",
       defaultName: "Lame Suprême",
       type: 4,
       enchants: [
@@ -70,6 +74,7 @@ const caisse = {
       ],
     },
     {
+      name: "&8&kt&r&f​‼&r&a​❇&r&f​‼&r&8&kt&r&a&l S&r&8&la&r&a&lb&r&8&lr&r&a&le&r&8&l d&r&a&le&r&8&l l&r&a'&r&8&lO&r&a&ll&r&8&li&r&a&lg&r&8&la&r&a&lr&r&8&lq&r&a&lu&r&8&le &r&8&kt&r&f​‼&r&a​❇&r&f​‼&r&8&kt&r",
       defaultName: "Sabre de l'Oligarque",
       type: 4,
       enchants: [
@@ -79,6 +84,7 @@ const caisse = {
       ],
     },
     {
+      name: "&8&kt&r&f☾&r&e۞&r&f☽&r&8&kt&r&e&l P&r&8&le&r&e&lr&r&8&lc&r&e&le&r&8&lu&r&e&ls&r&8&le &r&e&lS&r&8&lu&r&e&lp&r&8&lr&r&e&lê&r&8&lm&r&e&le &r&8&kt&r&f☾&r&e۞&r&f☽&r&8&kt&r",
       defaultName: "Perceuse Suprême",
       type: 7,
       enchants: [
@@ -88,6 +94,7 @@ const caisse = {
       ],
     },
     {
+      name: "&c&kt&r&4۞&r&c&kt&r&4&l M&r&c&la&r&4&lr&r&c&lt&r&4&le&r&c&la&r&4&lu&r&c-&r&4&lp&r&c&li&r&4&lq&r&c&lu&r&4&le&r&c&lu&r&4&lr&r&c&l d&r&4&le&r&c&l l&r&4'&r&c&lE&r&4&lm&r&c&lp&r&4&li&r&c&lr&r&4&le &r&c&kt&r&4۞&r&c&kt&r",
       defaultName: "Marteau-piqueur de l'Empire",
       type: 7,
       enchants: [
@@ -97,6 +104,7 @@ const caisse = {
       ],
     },
     {
+      name: "&f&kt&r&6⋱&r&e$&r&6⋰&r&f&kt&r&6&l F&r&e&lo&r&6&lr&r&e&le&r&6&lu&r&e&ls&r&6&le&r&e&l d&r&6&lu&r&e&l C&r&6&la&r&e&lp&r&6&li&r&e&lt&r&6&la&r&e&ll&r&6&li&r&e&ls&r&6&lt&r&e&le &r&f&kt&r&6⋱&r&e$&r&6⋰&r&f&kt&r",
       defaultName: "Foreuse du Capitaliste",
       type: 7,
       enchants: [
@@ -106,26 +114,31 @@ const caisse = {
       ],
     },
     {
+      name: "&8&kt&r&f☾&r&e۞&r&f☽&r&8&kt&r&e&l A&r&8&lr&r&e&lm&r&8&lu&r&e&lr&r&8&le&r&e&l S&r&8&lu&r&e&lp&r&8&lr&r&e&lê&r&8&lm&r&e&le &r&8&kt&r&f☾&r&e۞&r&f☽&r&8&kt&r",
       defaultName: "Armure Suprême",
       type: 0,
       enchants: ["&b&lProtection &r&3&l75&r", "&eSolidité &r&675&r"],
     },
     {
+      name: "&8&kt&r&f☾&r&e۞&r&f☽&r&8&kt&r&e&l A&r&8&lr&r&e&lm&r&8&lu&r&e&lr&r&8&le&r&e&l S&r&8&lu&r&e&lp&r&8&lr&r&e&lê&r&8&lm&r&e&le &r&8&kt&r&f☾&r&e۞&r&f☽&r&8&kt&r",
       defaultName: "Armure Suprême",
       type: 1,
       enchants: ["&b&lProtection &r&3&l75&r", "&eSolidité &r&675&r"],
     },
     {
+      name: "&8&kt&r&f☾&r&e۞&r&f☽&r&8&kt&r&e&l A&r&8&lr&r&e&lm&r&8&lu&r&e&lr&r&8&le&r&e&l S&r&8&lu&r&e&lp&r&8&lr&r&e&lê&r&8&lm&r&e&le &r&8&kt&r&f☾&r&e۞&r&f☽&r&8&kt&r",
       defaultName: "Armure Suprême",
       type: 2,
       enchants: ["&b&lProtection &r&3&l75&r", "&eSolidité &r&675&r"],
     },
     {
+      name: "&8&kt&r&f☾&r&e۞&r&f☽&r&8&kt&r&e&l A&r&8&lr&r&e&lm&r&8&lu&r&e&lr&r&8&le&r&e&l S&r&8&lu&r&e&lp&r&8&lr&r&e&lê&r&8&lm&r&e&le &r&8&kt&r&f☾&r&e۞&r&f☽&r&8&kt&r",
       defaultName: "Armure Suprême",
       type: 3,
       enchants: ["&b&lProtection &r&3&l75&r", "&eSolidité &r&675&r"],
     },
     {
+      name: "&f&kt&r&b✙&r&f&kt&r&b&l C&r&f&li&r&b&lm&r&f&le&r&b&lt&r&f&le&r&b&lr&r&f&lr&r&b&le&r&f&l d&r&b&lu&r&f&l S&r&b&lu&r&f&ll&r&b&lt&r&f&la&r&b&ln &r&f&kt&r&b✙&r&f&kt&r",
       defaultName: "Cimeterre du Sultan",
       type: 5,
       enchants: [
