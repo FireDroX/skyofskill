d83a1fbf4a8cb5d82795bb5a3e21045b141f5673
FireDroX <83990394+FireDroX@users.noreply.github.com>
2024-08-28 17:30:57 +0200

    Added 16 item(s) 📦 : BOX STARWARS

--- src/pages/caisses/Caisses.jsx
+++ src/pages/caisses/Caisses.jsx
@@ -14,6 +14,7 @@ import {
 } from "../../utils/functions";
 
 const caissesDates = [
+  "09_24",
   "08_24",
   "07_24",
   "06_24",
---
+++ src/utils/caisses/09_24.js
@@ -0,0 +1,178 @@
+/**
+ * HELMET : 0
+ * CHESTPLATE : 1
+ * LEGGINGS : 2
+ * BOOTS : 3
+ * SWORD : 4
+ * AXE : 5
+ * BOW : 6
+ * PICKAXE : 7
+ */
+
+const caisse = {
+  title: "SUPREME",
+  items: [
+    {
+      name: "&r&8&kt&r&8&kt&r&f&l✛&r&7&l☉&r&f&l✛&r&8&kt&r&8&kt&r&8&l C&r&7&lh&r&8&la&r&7&ls&r&8&ls&r&7&le&r&8&lu&r&7&lr &r&8&lT&r&7&lI&r&8&lE &r&8&kt&r&8&kt&r&f&l✛&r&7&l☉&r&f&l✛&r&8&kt&r&8&kt&r&r",
+      defaultName: "Chasseur TIE",
+      type: 6,
+      enchants: [
+        "&bPunch &r&3III&r",
+        "&eSolidité &r&610&r",
+        "&bInfinity &r&3I&r",
+      ],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&8&kt&r&8&kt&r&f&l✛&r&7&l☉&r&f&l✛&r&8&kt&r&8&kt&r&7&l A&r&f&lr&r&8&lm&r&7&lu&r&f&lr&r&8&le &r&7&lM&r&f&la&r&8&ln&r&7&ld&r&f&la&r&8&ll&r&7&lo&r&f&lr&r&8&li&r&7&le&r&f&ln&r&8&ln&r&7&le &r&8&kt&r&8&kt&r&f&l✛&r&7&l☉&r&f&l✛&r&8&kt&r&8&kt&r&7&l&r&r",
+      defaultName: "Armure Mandalorienne",
+      type: 0,
+      enchants: ["&b&lProtection &r&3&l85&r", "&eSolidité &r&685&r"],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&8&kt&r&8&kt&r&f&l✛&r&7&l☉&r&f&l✛&r&8&kt&r&8&kt&r&7&l A&r&f&lr&r&8&lm&r&7&lu&r&f&lr&r&8&le &r&7&lM&r&f&la&r&8&ln&r&7&ld&r&f&la&r&8&ll&r&7&lo&r&f&lr&r&8&li&r&7&le&r&f&ln&r&8&ln&r&7&le &r&8&kt&r&8&kt&r&f&l✛&r&7&l☉&r&f&l✛&r&8&kt&r&8&kt&r&7&l&r&r",
+      defaultName: "Armure Mandalorienne",
+      type: 1,
+      enchants: ["&b&lProtection &r&3&l85&r", "&eSolidité &r&685&r"],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&8&kt&r&8&kt&r&f&l✛&r&7&l☉&r&f&l✛&r&8&kt&r&8&kt&r&7&l A&r&f&lr&r&8&lm&r&7&lu&r&f&lr&r&8&le &r&7&lM&r&f&la&r&8&ln&r&7&ld&r&f&la&r&8&ll&r&7&lo&r&f&lr&r&8&li&r&7&le&r&f&ln&r&8&ln&r&7&le &r&8&kt&r&8&kt&r&f&l✛&r&7&l☉&r&f&l✛&r&8&kt&r&8&kt&r&7&l&r&r",
+      defaultName: "Armure Mandalorienne",
+      type: 2,
+      enchants: ["&b&lProtection &r&3&l85&r", "&eSolidité &r&685&r"],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&8&kt&r&8&kt&r&f&l✛&r&7&l☉&r&f&l✛&r&8&kt&r&8&kt&r&7&l A&r&f&lr&r&8&lm&r&7&lu&r&f&lr&r&8&le &r&7&lM&r&f&la&r&8&ln&r&7&ld&r&f&la&r&8&ll&r&7&lo&r&f&lr&r&8&li&r&7&le&r&f&ln&r&8&ln&r&7&le &r&8&kt&r&8&kt&r&f&l✛&r&7&l☉&r&f&l✛&r&8&kt&r&8&kt&r&7&l&r&r",
+      defaultName: "Armure Mandalorienne",
+      type: 3,
+      enchants: ["&b&lProtection &r&3&l85&r", "&eSolidité &r&685&r"],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&7&kt&r&f&l✧&r&8&l⚔&r&f&l✧&r&7&kt&r&8&l S&r&7&la&r&f&lb&r&8&lr&r&7&le &r&f&ld&r&8&lu &r&f&lS&r&8&lt&r&7&lr&r&f&lo&r&7&lm&r&f&lt&r&8&lr&r&7&lo&r&f&lo&r&8&lp&r&7&le&r&f&lr &r&7&kt&r&f&l✧&r&8&l⚔&r&f&l✧&r&7&kt&r&r",
+      defaultName: "Sabre du Stormtrooper",
+      type: 4,
+      enchants: [
+        "&aTranchant &r&284&r",
+        "&eSolidité &r&610&r",
+        "&dAura de Feu &r&510&r",
+      ],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&8&kt&r&8&kt&r&f&l✛&r&7&l☉&r&f&l✛&r&8&kt&r&8&kt&r&f&l F&r&7&lo&r&8&lr&r&f&le&r&7&lu&r&8&ls&r&f&le &r&7&lM&r&8&la&r&f&ln&r&7&ld&r&8&la&r&f&ll&r&7&lo&r&8&lr&r&f&li&r&7&le&r&8&ln&r&f&ln&r&7&le &r&8&kt&r&8&kt&r&f&l✛&r&7&l☉&r&f&l✛&r&8&kt&r&8&kt&r&r",
+      defaultName: "Foreuse Mandalorienne",
+      type: 7,
+      enchants: [
+        "&bEfficency &r&32800&r",
+        "&eSolidité &r&62800&r",
+        "&aFortune &r&22800&r",
+      ],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&8&kt&r&7&kt&r&f&lϟ&r&7&l✙&r&f&lϟ&r&7&kt&r&8&kt&r&7&l H&r&f&la&r&7&lc&r&f&lh&r&7&le&r&f&l L&r&7&la&r&f&ls&r&7&le&r&f&lr &r&7&ld&r&f&le &r&7&lS&r&f&lt&r&7&lr&r&f&lo&r&7&lm&r&f&lt&r&7&lr&r&f&lo&r&7&lo&r&f&lp&r&7&le&r&f&lr &r&8&kt&r&7&kt&r&f&lϟ&r&7&l✙&r&f&lϟ&r&7&kt&r&8&kt&r&r",
+      defaultName: "Hache Laser de Stormtrooper",
+      type: 5,
+      enchants: [
+        "&aTranchant &r&291&r",
+        "&eSolidité &r&610&r",
+        "&dAura de Feu &r&510&r",
+      ],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&4&kt&r&7&kt&r&f&l❰&r&4&l⚔&r&f&l❱&r&7&kt&r&4&kt&r&7&l X&r&4&l-&r&f&lW&r&7&lI&r&4&lN&r&f&lG &r&4&kt&r&7&kt&r&f&l❰&r&4&l⚔&r&f&l❱&r&7&kt&r&4&kt&r&r",
+      defaultName: "X-WING",
+      type: 6,
+      enchants: [
+        "&bPunch &r&3IV&r",
+        "&eSolidité &r&610&r",
+        "&bInfinity &r&3I&r",
+      ],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&3&kt&r&b&kt&r&f&lϟ&r&3&l☣&r&f&lϟ&r&b&kt&r&3&kt&r&3&l F&r&b&lo&r&3&lu&r&b&ld&r&3&lr&r&b&le &r&3&ld&r&b&le&r&3&ls &r&b&lS&r&3&li&r&b&lt&r&3&lh &r&3&kt&r&b&kt&r&f&lϟ&r&3&l☣&r&f&lϟ&r&b&kt&r&3&kt&r&r",
+      defaultName: "Foudre des Sith",
+      type: 7,
+      enchants: [
+        "&bEfficency &r&33000&r",
+        "&eSolidité &r&63000&r",
+        "&aFortune &r&23000&r",
+      ],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&8&kt&r&4&kt&r&c*&r&7✟&r&c*&r&4&kt&r&8&kt&r&4&l S&r&r&r&c&la&r&r&r&8&lb&r&r&r&4&lr&r&r&r&c&le &r&r&r&8&ld&r&r&r&4&le &r&r&r&c&lD&r&r&r&8&la&r&r&r&4&lr&r&r&r&c&lk &r&r&r&8&lV&r&r&r&4&la&r&r&r&c&ld&r&r&r&8&lo&r&r&r&4&lr&r&r &r&8&kt&r&4&kt&r&c*&r&7✟&r&c*&r&4&kt&r&8&kt&r&4&l&r&r",
+      defaultName: "Sabre de Dark Vador",
+      type: 4,
+      enchants: [
+        "&aTranchant &r&295&r",
+        "&eSolidité &r&610&r",
+        "&dAura de Feu &r&510&r",
+      ],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&5&kt&r&d&kt&r&f&kt&r&5&l*&r&d&l❤&r&5&l*&r&f&kt&r&d&kt&r&5&kt&r&d&l F&r&f&lr&r&5&lé&r&d&lg&r&f&la&r&5&lt&r&d&le &r&f&lM&r&5&lé&r&d&ld&r&f&li&r&5&lc&r&d&la&r&f&ll&r&5&le &r&5&kt&r&d&kt&r&f&kt&r&5&l*&r&d&l❤&r&5&l*&r&f&kt&r&d&kt&r&5&kt&r&r",
+      defaultName: "Frégate Médicale",
+      type: 6,
+      enchants: [
+        "&bPunch &r&3IV&r",
+        "&eSolidité &r&610&r",
+        "&bInfinity &r&3I&r",
+      ],
+      dontLeaveOnDeath: true,
+    },
+    {
+      name: "&r&8&kt&r&7&kt&r&b&l♦&r&3&l✷&r&b&l♦&r&7&kt&r&8&kt&r&3&l C&r&b&lh&r&7&la&r&8&lr&r&3&lg&r&b&le &r&7&lS&r&8&lo&r&3&ln&r&b&li&r&7&lq&r&8&lu&r&3&le &r&8&kt&r&7&kt&r&b&l♦&r&3&l✷&r&b&l♦&r&7&kt&r&8&kt&r&r",
+      defaultName: "Charge Sonique",
+      type: 7,
+      enchants: [
+        "&bEfficency &r&33300&r",
+        "&eSolidité &r&63300&r",
+        "&aFortune &r&23300&r",
+      ],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&2&kt&r&8&kt&r&7&l*&r&2&l✰&r&7&l*&r&8&kt&r&2&kt&r&8&l E&r&7&lt&r&2&lo&r&8&li&r&7&ll&r&2&le &r&8&lD&r&7&le &r&2&ll&r&8&la &r&7&lM&r&2&lo&r&8&lr&r&7&lt &r&2&kt&r&8&kt&r&7&l*&r&2&l✰&r&7&l*&r&8&kt&r&2&kt&r&r",
+      defaultName: "Etoile De la Mort",
+      type: 6,
+      enchants: [
+        "&bPunch &r&3V&r",
+        "&eSolidité &r&610&r",
+        "&bInfinity &r&3I&r",
+      ],
+      dontLeaveOnDeath: true,
+    },
+    {
+      name: "&r&4&kt&r&c&kt&r&7&l✡&r&8&kt&r&c&l✟&r&8&kt&r&7&l✡&r&c&kt&r&4&kt&r&4&l P&r&c&lu&r&7&ll&r&8&lv&r&4&lé&r&c&lr&r&7&li&r&8&ls&r&4&la&r&c&lt&r&7&le&r&8&lu&r&4&lr &r&c&lI&r&7&lm&r&8&lp&r&4&lé&r&c&lr&r&7&li&r&8&la&r&4&ll &r&4&kt&r&c&kt&r&7&l✡&r&8&kt&r&c&l✟&r&8&kt&r&7&l✡&r&c&kt&r&4&kt&r&r",
+      defaultName: "Pulvérisateur Impérial",
+      type: 7,
+      enchants: [
+        "&bEfficency &r&33500&r",
+        "&eSolidité &r&63500&r",
+        "&aFortune &r&23500&r",
+      ],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&b&kt&r&7&kt&r&8&kt&r&3&l⁑&r&8&l✧&r&3&l⁑&r&8&kt&r&7&kt&r&b&kt&r&3&l F&r&b&la&r&7&lu&r&8&lc&r&3&lo&r&b&ln &r&7&lM&r&8&li&r&3&ll&r&b&ll&r&7&le&r&8&ln&r&3&li&r&b&lu&r&7&lm &r&b&kt&r&7&kt&r&8&kt&r&3&l⁑&r&8&l✧&r&3&l⁑&r&8&kt&r&7&kt&r&b&kt&r&r",
+      defaultName: "Faucon Millenium",
+      type: 6,
+      enchants: [
+        "&bPunch &r&3VII&r",
+        "&eSolidité &r&610&r",
+        "&bInfinity &r&3I&r",
+      ],
+      dontLeaveOnDeath: true,
+    },
+  ],
+};
+
+export default caisse;
