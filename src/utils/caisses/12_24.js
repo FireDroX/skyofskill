ca2ad2be33bd16e77b9c5f5d607acf492ff441e4
FireDroX <83990394+FireDroX@users.noreply.github.com>
2024-11-24 15:26:38 +0100

    Caisse NOEL 2024

---
+++ src/utils/caisses/12_24.js
@@ -0,0 +1,167 @@
+/**
+ * HELMET : 0
+ * CHESTPLATE : 1
+ * LEGGINGS : 2
+ * BOOTS : 3
+ * SWORD : 4
+ * AXE : 5
+ * BOW : 6
+ * PICKAXE : 7
+ */
+
+const caisse = {
+  title: "SUPREME - CHRISTMASS",
+  items: [
+    {
+      name: "&r&e&kt&r&7&kt&r&e☥&r&6࿉&r&e☥&r&7&kt&r&e&kt&r&6&l P&r&e&la&r&7&li&r&6&ln&r&e&l d&r&7&l'é&r&6&lp&r&e&li&r&7&lc&r&6&le &r&e&kt&r&7&kt&r&e☥&r&6࿉&r&e☥&r&7&kt&r&e&kt",
+      defaultName: "Pain d'épice",
+      type: 6,
+      enchants: [
+        "&bPunch &r&3III&r",
+        "&eSolidité &r&610&r",
+        "&bInfinity &r&3I&r",
+      ],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&e&kt&r&7&kt&r&e☥&r&6࿉&r&e☥&r&7&kt&r&e&kt&r&6&l P&r&e&la&r&7&li&r&6&ln&r&e&l d&r&7&l'é&r&6&lp&r&e&li&r&7&lc&r&6&le &r&e&kt&r&7&kt&r&e☥&r&6࿉&r&e☥&r&7&kt&r&e&kt",
+      defaultName: "Pain d'épice",
+      type: 0,
+      enchants: ["&b&lProtection &r&3&l90&r", "&eSolidité &r&690&r"],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&e&kt&r&7&kt&r&e☥&r&6࿉&r&e☥&r&7&kt&r&e&kt&r&6&l P&r&e&la&r&7&li&r&6&ln&r&e&l d&r&7&l'é&r&6&lp&r&e&li&r&7&lc&r&6&le &r&e&kt&r&7&kt&r&e☥&r&6࿉&r&e☥&r&7&kt&r&e&kt",
+      defaultName: "Pain d'épice",
+      type: 1,
+      enchants: ["&b&lProtection &r&3&l90&r", "&eSolidité &r&690&r"],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&e&kt&r&7&kt&r&e☥&r&6࿉&r&e☥&r&7&kt&r&e&kt&r&6&l P&r&e&la&r&7&li&r&6&ln&r&e&l d&r&7&l'é&r&6&lp&r&e&li&r&7&lc&r&6&le &r&e&kt&r&7&kt&r&e☥&r&6࿉&r&e☥&r&7&kt&r&e&kt",
+      defaultName: "Pain d'épice",
+      type: 2,
+      enchants: ["&b&lProtection &r&3&l90&r", "&eSolidité &r&690&r"],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&e&kt&r&7&kt&r&e☥&r&6࿉&r&e☥&r&7&kt&r&e&kt&r&6&l P&r&e&la&r&7&li&r&6&ln&r&e&l d&r&7&l'é&r&6&lp&r&e&li&r&7&lc&r&6&le &r&e&kt&r&7&kt&r&e☥&r&6࿉&r&e☥&r&7&kt&r&e&kt",
+      defaultName: "Pain d'épice",
+      type: 3,
+      enchants: ["&b&lProtection &r&3&l90&r", "&eSolidité &r&690&r"],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&e&kt&r&7&kt&r&e☥&r&6࿉&r&e☥&r&7&kt&r&e&kt&r&6&l P&r&e&la&r&7&li&r&6&ln&r&e&l d&r&7&l'é&r&6&lp&r&e&li&r&7&lc&r&6&le &r&e&kt&r&7&kt&r&e☥&r&6࿉&r&e☥&r&7&kt&r&e&kt",
+      defaultName: "Pain d'épice",
+      type: 4,
+      enchants: [
+        "&aTranchant &r&289&r",
+        "&eSolidité &r&610&r",
+        "&dAura de Feu &r&510&r",
+      ],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&f&ki&r&b&ki&r&f࿈&r&b✧&r&f࿈&r&b&ki&r&f&ki&r&r &r&b&lL&r&f&la&r&b&ls&r&f&le&r&b&lr&r&f&l d&r&b&le&r&f&l J&r&b&lo&r&f&ly&r&b&la&r&f&lu&r&b&lx &r&f&ki&r&b&ki&r&f࿈&r&b✧&r&f࿈&r&b&ki&r&f&ki",
+      defaultName: "Laser de Joyaux",
+      type: 7,
+      enchants: [
+        "&bEfficency &r&33600&r",
+        "&eSolidité &r&63600&r",
+        "&aFortune &r&23600&r",
+      ],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&8&ki&r&c⊹&r&4☫&r&c⊹&r&8&ki&r&4&l M&r&c&la&r&4&lc&r&8&lh&r&c&le&r&8&lt&r&4&lt&r&c&le&r&4&l S&r&8&la&r&c&ln&r&8&lt&r&4&la &r&8&ki&r&c⊹&r&4☫&r&c⊹&r&8&ki",
+      defaultName: "Machette Santa",
+      type: 5,
+      enchants: [
+        "&aTranchant &r&296&r",
+        "&eSolidité &r&610&r",
+        "&dAura de Feu &r&510&r",
+      ],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&f&ki&r&7✵&r&f☸&r&7✵&r&f&ki&r&7&l P&r&f&li&r&7&ls&r&f&lt&r&7&lo&r&f&ll&r&7&le&r&f&lt&r&7&l à&r&f&l N&r&7&le&r&f&li&r&7&lg&r&f&le &r&f&ki&r&7✵&r&f☸&r&7✵&r&f&ki",
+      defaultName: "Pistolet à Neige",
+      type: 6,
+      enchants: [
+        "&bPunch &r&3IV&r",
+        "&eSolidité &r&610&r",
+        "&bInfinity &r&3I&r",
+      ],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&b&ki&r&9&l&ki&r&3❘&r&9&l&ki&r&b&ki&r&9 &l C&r&3&la&r&9&lr&r&b&lo&r&3&lt&r&b&lt&r&9&le&r&3&l d&r&9&l'O&r&b&ll&r&3&la&r&b&lf &r&b&ki&r&9&l&ki&r&3❘&r&9&l&ki&r&b&ki",
+      defaultName: "Carotte d'Olaf",
+      type: 7,
+      enchants: [
+        "&bEfficency &r&33800&r",
+        "&eSolidité &r&63800&r",
+        "&aFortune &r&23800&r",
+      ],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&4&ki&r&c☄&r&e✩&r&c☄&r&4&ki&r&4 &l S&r&c&la&r&e&lb&r&4&lr&r&c&le&r&e&l d&r&4&lu&r&c&l P&r&e&lè&r&4&lr&r&c&le&r&e&l F&r&4&lo&r&c&lu&r&e&le&r&4&lt&r&c&lt&r&e&la&r&4&lr&r&c&ld &r&4&ki&r&c☄&r&e✩&r&c☄&r&4&ki",
+      defaultName: "Sabre du Père Fouettard",
+      type: 4,
+      enchants: [
+        "&aTranchant &r&2100&r",
+        "&eSolidité &r&610&r",
+        "&dAura de Feu &r&510&r",
+      ],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&2&kt&r&a&kt&r&e☾&r&2⚚&r&e☽&r&a&kt&r&2&kt&r&2&l F&r&a&lo&r&2&lr&r&e&le&r&a&lu&r&e&ls&r&2&le&r&a&l d&r&2&lu&r&e&l L&r&a&lu&r&e&lt&r&2&li&r&a&ln &r&2&kt&r&a&kt&r&e☾&r&2⚚&r&e☽&r&a&kt&r&2&kt",
+      defaultName: "Foreuse du Lutin",
+      type: 7,
+      enchants: [
+        "&bEfficency &r&34100&r",
+        "&eSolidité &r&64100&r",
+        "&aFortune &r&24100&r",
+      ],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&8&ki&r&5☄&r&d❤&r&5☄&r&8&ki&r&d &l T&r&5&lr&r&d&la&r&8&li&r&5&ln&r&8&le&r&d&la&r&5&lu&r&d&l M&r&8&lè&r&5&lr&r&8&le&r&d&l N&r&5&lo&r&d&lë&r&8&ll &r&8&ki&r&5☄&r&d❤&r&5☄&r&8&ki",
+      defaultName: "Traineau Mère Noël",
+      type: 6,
+      enchants: [
+        "&bPunch &r&3VI&r",
+        "&eSolidité &r&610&r",
+        "&bInfinity &r&3I&r",
+      ],
+      dontLeaveOnDeath: true,
+    },
+    {
+      name: "&r&c&ki&r&f⊹&r&a『&r&f⊹&r&c&ki&r&a&l S&r&f&lu&r&c&lc&r&f&lr&r&a&le&r&f&l d&r&c&l'O&r&f&lr&r&a&lg&r&f&le &r&c&ki&r&f⊹&r&a『&r&f⊹&r&c&ki",
+      defaultName: "Sucre d'Orge",
+      type: 7,
+      enchants: [
+        "&bEfficency &r&34300&r",
+        "&eSolidité &r&64300&r",
+        "&aFortune &r&24300&r",
+      ],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&4&ki&r&c☄&r&e✩&r&c☄&r&4&ki&r&4 &l T&r&c&lr&r&e&la&r&4&li&r&c&ln&r&e&le&r&4&la&r&c&lu&r&e&l d&r&4&lu&r&c&l P&r&e&lè&r&4&lr&r&c&le&r&e&l F&r&4&lo&r&c&lu&r&e&le&r&4&lt&r&c&lt&r&e&la&r&4&lr&r&c&ld &r&4&ki&r&c☄&r&e✩&r&c☄&r&4&ki",
+      defaultName: "Traineau du Père Fouettard",
+      type: 6,
+      enchants: [
+        "&bPunch &r&3VII&r",
+        "&eSolidité &r&610&r",
+        "&bInfinity &r&3I&r",
+      ],
+      dontLeaveOnDeath: true,
+    },
+  ],
+};
+
+export default caisse;
