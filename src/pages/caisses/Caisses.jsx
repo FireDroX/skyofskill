2ebe4f8d236232832f67e1180d4f7bed2f25ca92
FireDroX <83990394+FireDroX@users.noreply.github.com>
2025-01-25 16:09:21 +0100

    Caisse GEMME 📦

--- src/pages/caisses/Caisses.jsx
+++ src/pages/caisses/Caisses.jsx
@@ -14,6 +14,7 @@ import {
 } from "../../utils/functions";
 
 const caissesDates = [
+  "02_25",
   "01_25",
   "12_24",
   "11_24",
---
+++ src/utils/caisses/02_25.js
@@ -0,0 +1,167 @@
+/**
+ * HELMET : 0
+ * CHESTPLATE : 1
+ * LEGGINGS : 2
+ * BOOTS : 3
+ * SWORD : 4
+ * AXE : 5
+ * BOW : 6
+ * PICKAXE : 7
+ */
+
+const caisse = {
+  title: "SUPREME - GEMME",
+  items: [
+    {
+      name: "&r&f&ki&r&8&kt&r&f⇔&r&8⊀⊁&r&f⇔&r&8&kt&r&f&ki&r&8&l A&r&f&lr&r&8&lb&r&f&la&r&8&ll&r&f&lè&r&8&lt&r&f&le&r&8&l e&r&f&ln&r&8&l O&r&f&ln&r&8&ly&r&f&lx &r&f&ki&r&8&kt&r&f⇔&r&8⊀⊁&r&f⇔&r&8&kt&r&f&ki",
+      defaultName: "Arbalète en Onyx",
+      type: 6,
+      enchants: [
+        "&bPunch &r&3III&r",
+        "&eSolidité &r&610&r",
+        "&bInfinity &r&3I&r",
+      ],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&f&ki&r&8&kt&r&f⇔&r&8⊀⊁&r&f⇔&r&8&kt&r&f&ki&r&8&l A&r&f&lr&r&8&lm&r&f&lu&r&8&lr&r&f&le&r&8&l e&r&f&ln&r&8&l O&r&f&ln&r&8&ly&r&f&lx &r&f&ki&r&8&kt&r&f⇔&r&8⊀⊁&r&f⇔&r&8&kt&r&f&ki",
+      defaultName: "Armure en Onyx",
+      type: 0,
+      enchants: ["&b&lProtection &r&3&l94&r", "&eSolidité &r&694&r"],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&f&ki&r&8&kt&r&f⇔&r&8⊀⊁&r&f⇔&r&8&kt&r&f&ki&r&8&l A&r&f&lr&r&8&lm&r&f&lu&r&8&lr&r&f&le&r&8&l e&r&f&ln&r&8&l O&r&f&ln&r&8&ly&r&f&lx &r&f&ki&r&8&kt&r&f⇔&r&8⊀⊁&r&f⇔&r&8&kt&r&f&ki",
+      defaultName: "Armure en Onyx",
+      type: 1,
+      enchants: ["&b&lProtection &r&3&l94&r", "&eSolidité &r&694&r"],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&f&ki&r&8&kt&r&f⇔&r&8⊀⊁&r&f⇔&r&8&kt&r&f&ki&r&8&l A&r&f&lr&r&8&lm&r&f&lu&r&8&lr&r&f&le&r&8&l e&r&f&ln&r&8&l O&r&f&ln&r&8&ly&r&f&lx &r&f&ki&r&8&kt&r&f⇔&r&8⊀⊁&r&f⇔&r&8&kt&r&f&ki",
+      defaultName: "Armure en Onyx",
+      type: 2,
+      enchants: ["&b&lProtection &r&3&l94&r", "&eSolidité &r&694&r"],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&f&ki&r&8&kt&r&f⇔&r&8⊀⊁&r&f⇔&r&8&kt&r&f&ki&r&8&l A&r&f&lr&r&8&lm&r&f&lu&r&8&lr&r&f&le&r&8&l e&r&f&ln&r&8&l O&r&f&ln&r&8&ly&r&f&lx &r&f&ki&r&8&kt&r&f⇔&r&8⊀⊁&r&f⇔&r&8&kt&r&f&ki",
+      defaultName: "Armure en Onyx",
+      type: 3,
+      enchants: ["&b&lProtection &r&3&l94&r", "&eSolidité &r&694&r"],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&f&ki&r&8&kt&r&f⇔&r&8⊀⊁&r&f⇔&r&8&kt&r&f&ki&r&8&l L&r&f&la&r&8&lm&r&f&le&r&8&l e&r&f&ln&r&8&l O&r&f&ln&r&8&ly&r&f&lx &r&f&ki&r&8&kt&r&f⇔&r&8⊀⊁&r&f⇔&r&8&kt&r&f&ki",
+      defaultName: "Lame en Onyx",
+      type: 4,
+      enchants: [
+        "&aTranchant &r&293&r",
+        "&eSolidité &r&610&r",
+        "&dAura de Feu &r&510&r",
+      ],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&3&kii&r&7⊤&r&f↟&r&7⊤&r&3&kii&r&7&l P&r&f&li&r&3&lo&r&7&lc&r&f&lh&r&3&le &r&f&ld&r&7&le&r&f&l Z&r&3&li&r&7&lr&r&f&lc&r&3&lo&r&7&ln&r&f&li&r&3&lu&r&7&lm &r&3&kii&r&7⊤&r&f↟&r&7⊤&r&3&kii",
+      defaultName: "Pioche de Zirconium",
+      type: 7,
+      enchants: [
+        "&bEfficency &r&34000&r",
+        "&eSolidité &r&64000&r",
+        "&aFortune &r&24000&r",
+      ],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&b&kii&r&7⊤&r&f↟&r&7⊤&r&b&kii&r&7&l H&r&f&la&r&b&lc&r&7&lh&r&f&le&r&b&l d&r&7&le&r&f&l Z&r&b&li&r&7&lr&r&f&lc&r&b&lo&r&7&ln&r&f&li&r&b&lu&r&7&lm &r&b&kii&r&7⊤&r&f↟&r&7⊤&r&b&kii",
+      defaultName: "Hache de Zirconium",
+      type: 5,
+      enchants: [
+        "&aTranchant &r&2100&r",
+        "&eSolidité &r&610&r",
+        "&dAura de Feu &r&510&r",
+      ],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&7&ki&r&f⊙&r&7⟨⟩&r&f⊙&r&7&ki&r&f&l A&r&7&lr&r&f&lc &r&7&ld&r&f&le &r&7&lG&r&f&la&r&7&ll&r&f&ll&r&7&li&r&f&lu&r&7&lm &r&7&ki&r&f⊙&r&7⟨⟩&r&f⊙&r&7&ki",
+      defaultName: "Arc de Gallium",
+      type: 6,
+      enchants: [
+        "&bPunch &r&3IV&r",
+        "&eSolidité &r&610&r",
+        "&bInfinity &r&3I&r",
+      ],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&e&ki&r&6&l&ki&r&e.&r&6✫&r&e.&r&6&l&ki&r&e&ki&r&6 &l Pi&r&e&lo&r&6&lch&r&e&le d&r&6&le T&r&e&lop&r&6&laz&r&e&le &r&e&ki&r&6&l&ki&r&e.&r&6✫&r&e.&r&6&l&ki&r&e&ki",
+      defaultName: "Pioche de Topaze",
+      type: 7,
+      enchants: [
+        "&bEfficency &r&34250&r",
+        "&eSolidité &r&64250&r",
+        "&aFortune &r&24250&r",
+      ],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&7&kii&r&c☆&r&4♨&r&c☆&r&7&kii&r&4 &l K&r&c&la&r&7&lt&r&4&la&r&c&ln&r&7&la&r&4&l d'&r&c&lO&r&7&lp&r&4&la&r&c&ll&r&7&le &r&7&kii&r&c☆&r&4♨&r&c☆&r&7&kii",
+      defaultName: "Katana d'Opale",
+      type: 4,
+      enchants: [
+        "&aTranchant &r&2104&r",
+        "&eSolidité &r&610&r",
+        "&dAura de Feu &r&510&r",
+      ],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&d&ki&r&8&kt&r&d⏏&r&5⚛&r&d⏏&r&8&kt&r&d&ki&r&d&l P&r&5&li&r&8&lo&r&d&lc&r&5&lh&r&8&le&r&d&l d'&r&5&lA&r&8&lm&r&d&lé&r&5&lt&r&8&lh&r&d&ly&r&5&ls&r&8&lt&r&d&le &r&d&ki&r&8&kt&r&d⏏&r&5⚛&r&d⏏&r&8&kt&r&d&ki",
+      defaultName: "Pioche d'Améthyste",
+      type: 7,
+      enchants: [
+        "&bEfficency &r&34500&r",
+        "&eSolidité &r&64500&r",
+        "&aFortune &r&24500&r",
+      ],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&f&ki&r&7꙳&r&3⭒&r&7꙳&r&f&ki&r&f &l  A&r&7&lr&r&3&lc&r&f&l e&r&7&ln&r&3&l P&r&f&ll&r&7&la&r&3&lt&r&f&li&r&7&ln&r&3&le &r&f&ki&r&7꙳&r&3⭒&r&7꙳&r&f&ki",
+      defaultName: "Arc en Platine",
+      type: 6,
+      enchants: [
+        "&bPunch &r&3VI&r",
+        "&eSolidité &r&610&r",
+        "&bInfinity &r&3I&r",
+      ],
+      dontLeaveOnDeath: true,
+    },
+    {
+      name: "&r&7&ki&r&8♱&r&1✧&r&8♱&r&7&ki&r&7&l C&r&8&lo&r&1&ll&r&7&ll&r&8&le&r&1&lc&r&7&lt&r&8&le&r&1&lu&r&7&ls&r&8&le &r&1&ld&r&7&le&r&8&l S&r&1&la&r&7&lp&r&8&lh&r&1&li&r&7&lr &r&7&ki&r&8♱&r&1✧&r&8♱&r&7&ki",
+      defaultName: "Collecteuse de Saphir",
+      type: 7,
+      enchants: [
+        "&bEfficency &r&34750&r",
+        "&eSolidité &r&64750&r",
+        "&aFortune &r&24750&r",
+      ],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&c&ki&r&7&ki&r&4⌖&r&4✜&r&4⌖&r&7&ki&r&c&ki&r&c &l A&r&7&lr&r&4&lb&r&c&la&r&7&ll&r&4&lè&r&c&lt&r&7&le &r&4&le&r&c&ln&r&7&l R&r&4&lu&r&c&lb&r&7&li&r&4&ls &r&c&ki&r&7&ki&r&4⌖&r&4✜&r&4⌖&r&7&ki&r&c&ki",
+      defaultName: "Arbalète en Rubis",
+      type: 6,
+      enchants: [
+        "&bPunch &r&3VIII&r",
+        "&eSolidité &r&610&r",
+        "&bInfinity &r&3I&r",
+      ],
+      dontLeaveOnDeath: true,
+    },
+  ],
+};
+
+export default caisse;
