af6ac4f9c8adbf7b59c7ef43e8f576659387e64d
FireDroX <83990394+FireDroX@users.noreply.github.com>
2025-02-24 11:47:32 +0100

    Box SUPREME - ARTISTIQUE 📦

--- src/pages/caisses/Caisses.jsx
+++ src/pages/caisses/Caisses.jsx
@@ -14,6 +14,7 @@ import {
 } from "../../utils/functions";
 
 const caissesDates = [
+  "03_25",
   "02_25",
   "01_25",
   "12_24",
---
+++ src/utils/caisses/03_25.js
@@ -0,0 +1,167 @@
+/**
+ * HELMET : 0
+ * CHESTPLATE : 1
+ * LEGGINGS : 2
+ * BOOTS : 3
+ * SWORD : 4
+ * AXE : 5
+ * BOW : 6
+ * PICKAXE : 7
+ */
+
+const caisse = {
+  title: "SUPREME - ARTISTIQUE",
+  items: [
+    {
+      name: "&r&d&ki&r&8&kt&r&e↕&r&d⿻&r&e↕&r&8&kt&r&d&ki&r&8&l C&r&d&lo&r&8&lm&r&e&lp&r&8&lo&r&d&ls&r&8&li&r&e&lt&r&8&li&r&d&lo&r&8&ln &r&8&lVII&r&e&lI &r&d&ki&r&8&kt&r&e↕&r&d⿻&r&e↕&r&8&kt&r&d&ki&r&8&l",
+      defaultName: "Composition VIII",
+      type: 6,
+      enchants: [
+        "&bPunch &r&3III&r",
+        "&eSolidité &r&610&r",
+        "&bInfinity &r&3I&r",
+      ],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&d&ki&r&8&kt&r&e↕&r&d⿻&r&e↕&r&8&kt&r&d&ki&r&8&l C&r&d&lo&r&8&lm&r&e&lp&r&8&lo&r&d&ls&r&8&li&r&e&lt&r&8&li&r&d&lo&r&8&ln &r&8&lVII&r&e&lI &r&d&ki&r&8&kt&r&e↕&r&d⿻&r&e↕&r&8&kt&r&d&ki&r&8&l",
+      defaultName: "Composition VIII",
+      type: 0,
+      enchants: ["&b&lProtection &r&3&l96&r", "&eSolidité &r&696&r"],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&d&ki&r&8&kt&r&e↕&r&d⿻&r&e↕&r&8&kt&r&d&ki&r&8&l C&r&d&lo&r&8&lm&r&e&lp&r&8&lo&r&d&ls&r&8&li&r&e&lt&r&8&li&r&d&lo&r&8&ln &r&8&lVII&r&e&lI &r&d&ki&r&8&kt&r&e↕&r&d⿻&r&e↕&r&8&kt&r&d&ki&r&8&l",
+      defaultName: "Composition VIII",
+      type: 1,
+      enchants: ["&b&lProtection &r&3&l96&r", "&eSolidité &r&696&r"],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&d&ki&r&8&kt&r&e↕&r&d⿻&r&e↕&r&8&kt&r&d&ki&r&8&l C&r&d&lo&r&8&lm&r&e&lp&r&8&lo&r&d&ls&r&8&li&r&e&lt&r&8&li&r&d&lo&r&8&ln &r&8&lVII&r&e&lI &r&d&ki&r&8&kt&r&e↕&r&d⿻&r&e↕&r&8&kt&r&d&ki&r&8&l",
+      defaultName: "Composition VIII",
+      type: 2,
+      enchants: ["&b&lProtection &r&3&l96&r", "&eSolidité &r&696&r"],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&d&ki&r&8&kt&r&e↕&r&d⿻&r&e↕&r&8&kt&r&d&ki&r&8&l C&r&d&lo&r&8&lm&r&e&lp&r&8&lo&r&d&ls&r&8&li&r&e&lt&r&8&li&r&d&lo&r&8&ln &r&8&lVII&r&e&lI &r&d&ki&r&8&kt&r&e↕&r&d⿻&r&e↕&r&8&kt&r&d&ki&r&8&l",
+      defaultName: "Composition VIII",
+      type: 3,
+      enchants: ["&b&lProtection &r&3&l96&r", "&eSolidité &r&696&r"],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&d&ki&r&8&kt&r&e↕&r&d⿻&r&e↕&r&8&kt&r&d&ki&r&8&l C&r&d&lo&r&8&lm&r&e&lp&r&8&lo&r&d&ls&r&8&li&r&e&lt&r&8&li&r&d&lo&r&8&ln &r&8&lVII&r&e&lI &r&d&ki&r&8&kt&r&e↕&r&d⿻&r&e↕&r&8&kt&r&d&ki&r&8&l",
+      defaultName: "Composition VIII",
+      type: 4,
+      enchants: [
+        "&aTranchant &r&295&r",
+        "&eSolidité &r&610&r",
+        "&dAura de Feu &r&510&r",
+      ],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&8&kii&r&7⚑&r&f☀&r&7⚑&r&8&kii&r&7&l V&r&8&le&r&f&li&r&7&ll&r&8&ll&r&f&le&r&7&l d&r&8&le&r&f&l n&r&7&lu&r&8&li&r&f&lt &r&8&kii&r&7⚑&r&f☀&r&7⚑&r&8&kii",
+      defaultName: "Veille de nuit",
+      type: 7,
+      enchants: [
+        "&bEfficency &r&34250&r",
+        "&eSolidité &r&64250&r",
+        "&aFortune &r&24250&r",
+      ],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&8&kii&r&7⚑&r&f☀&r&7⚑&r&8&kii&r&7&l V&r&8&le&r&f&li&r&7&ll&r&8&ll&r&f&le&r&7&l d&r&8&le&r&f&l n&r&7&lu&r&8&li&r&f&lt &r&8&kii&r&7⚑&r&f☀&r&7⚑&r&8&kii",
+      defaultName: "Veille de nuit",
+      type: 5,
+      enchants: [
+        "&aTranchant &r&2102&r",
+        "&eSolidité &r&610&r",
+        "&dAura de Feu &r&510&r",
+      ],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&f&ki&r&1≭&r&a⊀⊁&r&1≭&r&f&ki&r&1&l Su&r&a&lr l&r&f&la v&r&1&lil&r&a&lle &r&f&ki&r&1≭&r&a⊀⊁&r&1≭&r&f&ki",
+      defaultName: "",
+      type: 6,
+      enchants: [
+        "&bPunch &r&3IV&r",
+        "&eSolidité &r&610&r",
+        "&bInfinity &r&3I&r",
+      ],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&a&ki&r&e&l&ki&r&a!&r&2❁&r&a!&r&e&l&ki&r&a&ki&r&2 &l L&r&a&la &r&e&lm&r&2&la&r&a&li&r&e&lt&r&2&lr&r&a&le&r&e&ls&r&2&ls&r&a&le &r&a&ki&r&e&l&ki&r&a!&r&2❁&r&a!&r&e&l&ki&r&a&ki",
+      defaultName: "La maitresse",
+      type: 7,
+      enchants: [
+        "&bEfficency &r&34500&r",
+        "&eSolidité &r&64500&r",
+        "&aFortune &r&24500&r",
+      ],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&f&kii&r&8ᔐ&r&4⚔&r&8ᔐ&r&f&kii&r&8 &l L'&r&4&lA&r&f&ls&r&8&ls&r&4&la&r&f&ls&r&8&ls&r&4&li&r&f&ln&r&8&l M&r&4&le&r&f&ln&r&8&la&r&4&lc&r&f&lé &r&f&kii&r&8ᔐ&r&4⚔&r&8ᔐ&r&f&kii",
+      defaultName: "L'Assassin Menacé",
+      type: 4,
+      enchants: [
+        "&aTranchant &r&2106&r",
+        "&eSolidité &r&610&r",
+        "&dAura de Feu &r&510&r",
+      ],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&f&ki&r&4&kt&r&f✝&r&7✸&r&f✝&r&4&kt&r&f&ki&r&f&l A&r&7&l l'&r&4&la&r&f&lb&r&7&lr&r&4&li&r&f&l d&r&7&lu&r&4&l b&r&f&le&r&7&ls&r&4&lo&r&f&li&r&7&ln &r&f&ki&r&4&kt&r&f✝&r&7✸&r&f✝&r&4&kt&r&f&ki",
+      defaultName: "A l'abri du besoin",
+      type: 7,
+      enchants: [
+        "&bEfficency &r&34750&r",
+        "&eSolidité &r&64750&r",
+        "&aFortune &r&24750&r",
+      ],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&c&ki&r&e※&r&6❈&r&e※&r&c&ki&r&e &l L&r&6&le&r&c&l S&r&e&le&r&6&lm&r&c&le&r&e&lu&r&6&lr &r&c&ki&r&e※&r&6❈&r&e※&r&c&ki",
+      defaultName: "Le Semeur",
+      type: 6,
+      enchants: [
+        "&bPunch &r&3VI&r",
+        "&eSolidité &r&610&r",
+        "&bInfinity &r&3I&r",
+      ],
+      dontLeaveOnDeath: true,
+    },
+    {
+      name: "&r&3&ki&r&6&ki&r&f&l&kt&r&3:&r&6◤◥&r&3:&r&f&kt&r&6&ki&r&3&ki&r&3&l N&r&f&li&r&6&lg&r&3&lh&r&f&lt&r&6&lh&r&3&la&r&f&lw&r&6&lk&r&3&ls &r&3&ki&r&6&ki&r&f&l&kt&r&3:&r&6◤◥&r&3:&r&f&kt&r&6&ki&r&3&ki",
+      defaultName: "Nighthawks",
+      type: 7,
+      enchants: [
+        "&bEfficency &r&35000&r",
+        "&eSolidité &r&65000&r",
+        "&aFortune &r&25000&r",
+      ],
+      dontLeaveOnDeath: false,
+    },
+    {
+      name: "&r&8&kii&r&eᔐ&r&9☾&r&eᔑ&r&8&kii&r&9 &l N&r&e&lu&r&8&li&r&9&lt&r&e&l é&r&8&lt&r&9&lo&r&e&li&r&8&ll&r&9&lé&r&e&le &r&8&kii&r&eᔐ&r&9☾&r&eᔑ&r&8&kii",
+      defaultName: "Nuit étoilée",
+      type: 6,
+      enchants: [
+        "&bPunch &r&3VIII&r",
+        "&eSolidité &r&610&r",
+        "&bInfinity &r&3I&r",
+      ],
+      dontLeaveOnDeath: true,
+    },
+  ],
+};
+
+export default caisse;
